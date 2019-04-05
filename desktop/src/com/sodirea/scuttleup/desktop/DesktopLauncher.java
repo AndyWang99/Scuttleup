a2f8de5007ab9a2765b6358d1257402c1bc6c2d6
AndyWang99 <sam.andy.wang@gmail.com>
2019-04-05 18:35:19 -0400

    init setup libgdx

---
+++ desktop/src/com/sodirea/scuttleup/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.sodirea.scuttleup.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sodirea.scuttleup.Scuttleup;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Scuttleup(), config);
+	}
+}
