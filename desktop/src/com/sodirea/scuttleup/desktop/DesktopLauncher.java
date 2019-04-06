2792243a498e6f09ab42d74f7f570e7214baf45d
AndyWang99 <sam.andy.wang@gmail.com>
2019-04-05 21:52:48 -0400

    default to fullscreen

--- desktop/src/com/sodirea/scuttleup/desktop/DesktopLauncher.java
+++ desktop/src/com/sodirea/scuttleup/desktop/DesktopLauncher.java
@@ -1,12 +1,15 @@
 package com.sodirea.scuttleup.desktop;
 
+import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.sodirea.scuttleup.Scuttleup;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
+		Graphics.DisplayMode displayMode = LwjglApplicationConfiguration.getDesktopDisplayMode();
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.setFromDisplayMode(displayMode);
 		new LwjglApplication(new Scuttleup(), config);
 	}
 }
