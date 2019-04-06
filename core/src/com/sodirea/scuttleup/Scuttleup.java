aeb1f3f31d7fa08fd91c8925bb504d278368a371
AndyWang99 <sam.andy.wang@gmail.com>
2019-04-05 23:58:06 -0400

    menu has groundwork

--- core/src/com/sodirea/scuttleup/Scuttleup.java
+++ core/src/com/sodirea/scuttleup/Scuttleup.java
@@ -5,29 +5,32 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.sodirea.scuttleup.states.GameStateManager;
+import com.sodirea.scuttleup.states.MenuState;
 
 public class Scuttleup extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	private SpriteBatch sb;
+	private GameStateManager gsm;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		sb = new SpriteBatch();
+		gsm = new GameStateManager();
+		gsm.push(new MenuState(gsm));
+
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		gsm.update(Gdx.graphics.getDeltaTime());
+		gsm.render(sb);
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		sb.dispose();
+
 	}
 }
